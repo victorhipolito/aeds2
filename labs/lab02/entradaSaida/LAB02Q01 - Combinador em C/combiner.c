f04d126a638d22cf8b9aff42c19390962717c0b4
Victor Hipolito <victorhipolito@hotmail.com.br>
2026-02-26 12:12:35 -0300

    LAB02Q01 combiner.c completamente funcional. Re

--- labs/lab02/entradaSaida/LAB02Q01 - Combinador em C/combiner.c
+++ labs/lab02/entradaSaida/LAB02Q01 - Combinador em C/combiner.c
@@ -2,42 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void  combinador(char *parteA, char *parteB, char *recebedor){
-
+void combinador(char *parteA, char *parteB){
+	// Guarda os tamanhos da parteA e parteB para usos na função
 	int tA = strlen(parteA), tB = strlen(parteB);
-	recebedor =(char*)realloc(recebedor, (sizeof(char)*(tA+tB+1)));
-	recebedor[strlen(recebedor)-1] = '\0';
+	char *final =(char*)malloc(sizeof(char)*(tA+tB+1));
+	// Redimensiona o tamanho do ponteiro que receberá a combinação para exatamente o tamanho combinado
+	final[tA+tB] = '\0';
+	//loop de inserção
 	int local = 0;
 	for (int i = 0; i < tA; i++){
-		recebedor[local] = parteB[i];
+		final[local] = parteB[i];
 		local++;
-		recebedor[local] = parteA[i];
+		final[local] = parteA[i];
 		local++;
 	}
 	for (int i = tA; i < tB; i++){
-		recebedor[local] = parteB[i];
+		final[local] = parteB[i];
 		local++;
 	}
+	printf("%s\n", final);
+	free(final);
 }
 
 int main(){
 	char *p1 = (char*)malloc(200*sizeof(char));
 	char *p2 = (char*)malloc(200*sizeof(char));
-	char *junto = (char*)malloc(sizeof(char));
 
-	while(scanf("%s", p1) != EOF){
-		scanf("%s", p2);
-		(strlen(p1)<=strlen(p2)) ? combinador(p1,p2,junto) : combinador(p2,p1,junto); 	
-		for (int i = 0; i < strlen(junto); i++){
-			printf("%c", junto[i]);
-		}
-		printf("\n");
+	while(scanf(" %s %s", p1, p2) != EOF){
+		if(strlen(p1)<=strlen(p2)){combinador(p1,p2);}
+	        else{combinador(p2,p1);} 	
 	}
 
 	free(p1);
 	free(p2);
-	free(junto);
 	return 0;
-
 }
 
