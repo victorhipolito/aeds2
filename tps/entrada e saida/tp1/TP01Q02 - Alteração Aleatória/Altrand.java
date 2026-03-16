5537b228204b8bcbb3a6b36ba68ff1b35ab8e4d6
victorhipolito <victorhipolito@hotmail.com.br>
2026-03-16 18:39:49 -0300

    TP01Q02 - Correções de erros (a comentar)

--- tps/entrada e saida/tp1/TP01Q02 - Alteração Aleatória/Altrand.java
+++ tps/entrada e saida/tp1/TP01Q02 - Alteração Aleatória/Altrand.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Math;
+import java.lang.Math;
 
 class Aleatoria {
 	
-	String alteracao(String original, char charorig, char charfinal){
+	public static String alteracao(String original, char charorig, char charfinal){
 		int tam = original.length();
 		char[] alt = new char[tam];
 		String alterado;
-
-		
 		
 		for(int i = 0; i < tam; i++){
-		(original.charAt(i)==charorig) ? alt[i] = charfinal : alt[i] = original.charAt(i);
+		alt[i] = (original.charAt(i)==charorig) ? charfinal : original.charAt(i);
 		}
 		
 		alterado = new String(alt);
@@ -20,23 +18,19 @@ class Aleatoria {
 	}
 	
 	public static void main(String args[]){
-		Scanner  sc = new Scanner();
+		Scanner sc = new Scanner(System.in);
 		Random gerador = new Random();
 		gerador.setSeed(4);
 		String original = sc.nextLine();
 		char charorig, charfinal;		
 
 		while(!(original.equals("FIM"))){	
-			charorig = 'a'+(Math.abs(gerador.nextInt())%26);	
-			charfinal = 'a'+(Math.abs(gerador.nextInt())%26);
-			
+			charorig = (char)('a' + (Math.abs(gerador.nextInt())%26));	
+			charfinal = (char)('a' + (Math.abs(gerador.nextInt())%26));
+				
 			System.out.println(alteracao(original, charorig, charfinal));
 			
 			original = sc.nextLine();	
 		}
-		
-		
 	}
-	
-	
 }
