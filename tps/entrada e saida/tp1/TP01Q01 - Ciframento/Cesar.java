0bd0c77c178ea6db6b2609d11e2c0c6316c44b77
Victor Hipolito <victorhipolito@hotmail.com.br>
2026-03-19 08:04:01 -0300

    Questão 1 - Cifra completa

--- tps/entrada e saida/tp1/TP01Q01 - Ciframento/Cesar.java
+++ tps/entrada e saida/tp1/TP01Q01 - Ciframento/Cesar.java
@@ -5,22 +5,33 @@ class Cesar{
 	private static int chave;
 	private static void setChave(int numero){chave=numero%26;}
 	
-	public static String cifra(String original, int key){
+	public static String cifra(String original){
 		//Declaração de Variaveis
 		int tam = original.length();
 		String criptografado;
 		char[] manip = new char[tam];
-			
+		
+		
 		//loop de inserção em manip
 		for(int i = 0; i < tam; i++){
-			int val =
-			manip[i] = original.charAt(i) +
+			//int val=0;
+			//if (original.charAt(i) >= 'a'){val = 97;}
+			//else {val = 65;}
+			manip[i] = (char)((original.charAt(i)+chave)%127);
 		}	
+		criptografado = new String(manip);
 	
 		return criptografado;
 	}			
 
-
-
-
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		setChave(3);
+		String original = sc.nextLine();
+		while (!(original.equals("FIM"))){
+			System.out.println(cifra(original));
+			original = sc.nextLine();
+		}
+		
+	}
 }
