89897d9134eb60ccfc7f0f9af6eb86e6d4473431
Victor Hipolito <victorhipolito@hotmail.com.br>
2026-03-19 10:26:12 -0300

    Questao 4 -> Requer correções minimas

--- tps/entrada e saida/tp1/TP01Q04 - Inversão de String/inversao.c
+++ tps/entrada e saida/tp1/TP01Q04 - Inversão de String/inversao.c
@@ -1,8 +1,34 @@
-include <stdio.h>
-include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int tamString(char * string){
+	int tamanho = 0;
+	for (int i = 0; i < sizeof(string); i++){
+		(string[i] == '\0') ? i = sizeof(string) : tamanho++;
+	}
+	return tamanho;
+}
 
 char* inversao(char* original){
-	char* invertido = (char*)malloc(sizeof(original)*sizeof(char));
-	 
+	int t = tamString(original);
+	char* invertido = (char*)malloc((t)*sizeof(char));
+	for (int i = 1; i <= t; i++){invertido[i-1] = original[t-i];}
+	invertido[t] = '\0';
+	
+	return invertido;
+}
+
+int main(){
+	char * original = (char*)malloc(200*sizeof(char));
+	char * invertido;
+	while(scanf(" %s", original) != EOF){
 		
-}	
+		invertido = inversao(original);
+		printf("%s\n", invertido);
+		
+		free(invertido);
+	}
+	
+	free(original);
+	return 0;
+}
