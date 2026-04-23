f90369c9d74431975213f66484e4a6d86e9de5a6
Victor Hipolito <victorhipolito@hotmail.com.br>
2026-04-23 14:28:19 -0400

    Modelagem quase pronta

--- tps/entrada e saida/tp2/TP02Q01 - Modelagem em Java/Modelagem.java
+++ tps/entrada e saida/tp2/TP02Q01 - Modelagem em Java/Modelagem.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.File;
 
 // Classe manipulação guardara funções de manipulação de valores comuns de duas ou mais classes do projeto.
 class Manipulacao{
@@ -12,11 +13,12 @@ class Manipulacao{
 		}
 		return conv;
 	}
-	
+
 	// Conta a quantidade de vezes que um caracter aparece numa string
 	public static int countChar(char c, String s){
 		int qts = 0;
 		for (int i = 0; i < s.length(); i++) if (s.charAt(i) == c) qts++;
+		return qts;
 	}
 }	
 
@@ -119,7 +121,20 @@ class Restaurante{
 	private static Data data_abertura;
 	private static boolean aberto;
 	
-	private static Restaurante(int i, String n, String cid, int cap, double aval, String[] t_c, int f_p, Hora h_a, Hora h_f, Data d_a, boolean a){
+	public int getId(){return id;}	
+	private String getNome(){return nome;}
+	private String getCidade(){return cidade;}
+	private int getCapacidade(){return capacidade;}
+	private double getAval(){return avaliacao;}
+	private String[] getTC(){return tipos_cozinha;}
+	private int getFP(){return faixa_preco;}
+	private Hora getHA(){return horario_abertura;}
+	private Hora getHF(){return horario_fechamento;}
+	private Data getDA(){return data_abertura;}
+	private boolean getAberto(){return aberto;}
+
+
+	private Restaurante(int i, String n, String cid, int cap, double aval, String[] t_c, int f_p, Hora h_a, Hora h_f, Data d_a, boolean a){
 		id = i;
 		nome = n;
 		cidade = cid;
@@ -135,77 +150,132 @@ class Restaurante{
 
 
 	// Separa a substring dos tipos_cozinha pelo separador (;)
-	public static void parseTipos(String s){
+	public static String[] parseTipos(String s){
 		// Recebe a quantidade de tipos de cozinha que há e inicializa com base nisso a array tipos_cozinha
 		int qtTipos = Manipulacao.countChar(';', s);
-		tipos_cozinha = new String[qtTipos];
+		String[] tp = new String[qtTipos];
 		int loc = 0;
-		tipos_cozinha[0] = new String();
+		for (int i = 0; i < qtTipos; i++) tp[i] = new String();
 		for(int i = 0; i < s.length(); i++){
 			if (s.charAt(i) == ';'){
 				loc++;
-				tipos_cozinha[loc] = new String();
 			}
 			else{
-				tipos_cozinha[loc] += s.charAt(i);
+				tp[loc] += s.charAt(i);
 			}
 		}
+		return tp;
 	}
 	
 	public static Restaurante parseRestaurante(String s){
-		// Dividir a string em 11 substrings;
-		// Tratar cada substring com parse(tipo)
-		String[] auxiliar = new String[11];
-		int pos = 0;
-		auxiliar[0] = new String();
-		for(int i = 0; i < s.length(); i++){
-			if (s.charAt(i) == ','){
-				pos++;
-				auxiliar[pos] = new String();
-			}
-			else{
-				auxiliar[pos] += s.charAt(i);
-			}
-		}
+		Scanner sc = new Scanner(s);
+		sc.useDelimiter(",");
+		String tmp;
 		// id
-		id = Integer.parseInt(auxiliar[0]);
+		id = sc.nextInt();
 		// nome
-		nome = new String(auxiliar[1]);
+		nome = sc.next();
 		// cidade
-		cidade = new String(auxiliar[2]);
+		cidade = sc.next();
 		// capacidade
-		capacidade = Integer.parseInt(auxiliar[3]);
+		capacidade = sc.nextInt();
 		// avaliacao
-		avaliacao = Double.parseDouble(auxiliar[4]);
+		tmp = sc.next();
+		avaliacao = Double.parseDouble(tmp);
 		// tipos_cozinha
-		tipos_cozinha = Restaurante.addTipos(auxiliar[5]);
+		tmp = sc.next();
+		tipos_cozinha = parseTipos(tmp);
 		// faixa_preço
-		faixa_preco = auxiliar[6].length();	
+		tmp = sc.next();
+		faixa_preco = tmp.length();	
 		// horario_abertura
-		horario_abertura = Hora.parseHora(auxiliar[7]);
+		tmp = sc.next();
+		horario_abertura = Hora.parseHora(tmp);
 		// horario_fechamento
-		horario_fechamento = Hora.parseHora(auxiliar[8]);
+		tmp = sc.next();
+		horario_fechamento = Hora.parseHora(tmp);
 		// data_abertura
-		data_abertura = Data.parseData(auxiliar[9]);
+		tmp = sc.next();
+		data_abertura = Data.parseData(tmp);
 		// aberto
-		aberto = (auxiliar[10].charAt(0) == 't');
+		tmp = sc.next();
+		aberto = (tmp.charAt(0) == 't');
+		sc.close();
 		return new Restaurante(id, nome, cidade, capacidade, avaliacao, tipos_cozinha, faixa_preco, horario_abertura, horario_fechamento, data_abertura, aberto);
 	}
 
 	public static String formatar(){
-		String s = new String();
-		s = String.format();
+		String s = new String("[");
+		// Elem. já formatados
+		s += String.format("%d ## %s ## %s ## %.1lf ## [", id, nome, cidade, capacidade, avaliacao);
+		//tipos_cozinha
+		for (int i = 0; i < tipos_cozinha.length; i++){
+			s += tipos_cozinha[i];
+			if (i != tipos_cozinha.length - 1) s += ",";
+			else s += "] ## ";
+		}
+		//faixa_preco
+		for (int i = 0; i < faixa_preco; i++) s += "$";
+		// Horas e datas
+		s += String.format(" ## %s-%s ## %s ## ", horario_abertura.formatar(), horario_fechamento.formatar(), data_abertura.formatar());
+		// Booleano
+		s += (aberto) ? "true]" : "false]";
+
 		return s;
 	}
 }
 
-//class ColecaoRestaurantes{
+class ColecaoRestaurantes{
+	private int tamanho;
+	private static Restaurante[] restaurantes;
 
-//}
+	public int getTamanho(){return tamanho;}
+	
+	public static Restaurante[] getRestaurantes(){return restaurantes;}
+	
+	public int findRest(int qual){
+		for (int i = 0; i < getTamanho(); i++){
+			if(restaurantes[i].getId() == qual) return i;
+		}
+		return -1;
+	}
+	//Path = /tmp/restaurantes.csv
+	public void lerCsv(String path){
+		File arquivo = new File(path);
+		int n = 0;
+		try (Scanner ler = new Scanner(arquivo)){
+			while(ler.hasNextLine()){n++; ler.next();}
+			ler.reset();
+			tamanho = n;
+			restaurantes = new Restaurante[n];
+			for (int i = 0; i < n; n++){restaurantes[i] = Restaurante.parseRestaurante(ler.nextLine());}
+			ler.close();
+		}
+		catch (Exception e) {System.out.println("Erro: arquivo não encontrado");}
+	}
+
+	private ColecaoRestaurantes(String cam){lerCsv(cam);}
+
+	public static ColecaoRestaurantes lerCsv(){return new ColecaoRestaurantes("/tmp/restaurantes.csv");}
+}
 
 class Modelagem{
 
+
+
 	public static void main(String args[]){
-		int i = 0;
+		Scanner sc = new Scanner(System.in);
+		ColecaoRestaurantes col = ColecaoRestaurantes.lerCsv();
+		Restaurante[] rests = ColecaoRestaurantes.getRestaurantes();
+		int r = -2;
+		int pos;
+		while(r != -1){
+			r = sc.nextInt();
+			pos = col.findRest(r);
+			if (pos != -1) System.out.println(rests[pos].formatar());
+			else System.out.println("Erro: Não achou o restaurante");
+		}
+		sc.close();	
 	}
+
 }
